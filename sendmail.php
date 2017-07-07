c1c2973e0e300bde50506906f658a4d60e0f01d6
dwaynedsouza <dwayned0456@gmail.com>
2017-07-06 21:18:01 -0400

    July 6th

---
+++ sendmail.php
@@ -0,0 +1,46 @@
+<?php
+
+//if "email" variable is filled out, send email
+
+  if (isset($_POST['email']))  {
+
+  
+
+  //Email information
+
+  $admin_email = "admin@iubism.org";
+
+$fname=$_POST['name'];
+
+  $email = $_POST['email'];
+
+ $phone= $_POST['phone'];
+
+  $subject = $_POST['subject'];
+
+  $message = $_POST['message'];
+
+  $newline = '
+';
+
+  //send email
+
+  mail($admin_email, "$subject","Name:" . $fname . $newline. "Phone No:" . $phone .$newline. $message,"From:" . $email);
+
+  
+
+  //Email response
+
+  echo "Thank you for contacting us!";
+
+  }
+
+  
+
+ 
+?>
+
+
+
+  
+
