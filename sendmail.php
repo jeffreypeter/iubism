b0639c7699639d13623d7ec8d1f242493b30dc04
jeffravi <jeffravi@iu.edu>
2017-08-02 00:16:30 -0400

    Fixed Apple event

--- sendmail.php
+++ sendmail.php
@@ -1,46 +1,110 @@
 <?php
 
+
+
 //if "email" variable is filled out, send email
+
+
 
   if (isset($_POST['email']))  {
 
+
+
   
+
+
 
   //Email information
 
-  $admin_email = "elan@ifmusa.org";
+
+
+  $admin_email = "admin@iubism.org";
+
+
 
 $fname=$_POST['name'];
 
+
+
   $email = $_POST['email'];
+
+$headers = "From: ".$email."\r\n";
 
  $phone= $_POST['phone'];
 
+$headers .= "BCC: admin@iubism.org\r\n";
+
   $subject = $_POST['subject'];
+
+
 
   $message = $_POST['message'];
 
+
+
   $newline = '
+
 ';
 
+
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+    CURLOPT_POST => 1,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_POSTFIELDS => [
+        'secret' => '6LcUmiwUAAAAADP_hyrHWcjqPQRNGZhYBvLkeMo7',
+        'response' => $_POST['g-recaptcha-response'],
+        ],
+]);
+$response = json_decode(curl_exec($curl));
   //send email
 
-  mail($admin_email, "$subject","Name:" . $fname . $newline. "Phone No:" . $phone .$newline. $message,"From:" . $email);
+if ($response->success) {
+    //your actual code.
 
-  
 
-  //Email response
 
-  echo "Thank you for contacting us!";
+ if(mail($admin_email, "$subject","Name:" . $fname . $newline. "Phone No:" . $phone .$newline. $message,$headers))
+
+
+ {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+}
+else
+{
+died("Recaptcha missing.");
+}
 
   }
 
+
+
   
 
+
+
  
+
 ?>
 
 
 
+
+
+
+
   
+
+
 
